29b84ee89987562f951f01fd5db1b7e98ff64b6d
tasos <tasos.xroneas@gmail.com>
2025-06-30 14:14:13 +0300

    Dockerfile docker-compose.yaml configurations and minor fixes

--- src/main/java/com/tchroneas/matchapplication/dto/MatchDTO.java
+++ src/main/java/com/tchroneas/matchapplication/dto/MatchDTO.java
@@ -3,9 +3,9 @@ package com.tchroneas.matchapplication.dto;
 import com.tchroneas.matchapplication.helpers.Sport;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class MatchDTO {
 
@@ -15,9 +15,13 @@ public class MatchDTO {
     private String description;
 
     @NotNull(message = "Match date is required")
-    private LocalDate matchDate;
+    @Pattern(
+            regexp = "^\\d{4}-\\d{2}-\\d{2}$",
+            message = "Match date must be in yyyy-MM-dd format")
+    private String matchDate;
 
     @NotNull(message = "Match time is required")
+    @Pattern(regexp = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$", message = "Match time must be in HH:mm format")
     private String matchTime;
 
     @NotBlank(message = "Team A is required")
@@ -45,11 +49,11 @@ public class MatchDTO {
         this.description = description;
     }
 
-    public LocalDate getMatchDate() {
+    public String getMatchDate() {
         return matchDate;
     }
 
-    public void setMatchDate(LocalDate matchDate) {
+    public void setMatchDate(String matchDate) {
         this.matchDate = matchDate;
     }
 
--- src/main/java/com/tchroneas/matchapplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tchroneas/matchapplication/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.tchroneas.matchapplication.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
@@ -9,17 +11,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.DateTimeException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleEntityNotFound(EntityNotFoundException ex) {
         ApiError apiError = new ApiError("404","Entity not found");
+        log.error("an error occured",ex);
         return apiError;
     }
 
@@ -31,6 +36,7 @@ public class GlobalExceptionHandler {
             sb.append(fieldError.getField()).append(":").append(fieldError.getDefaultMessage()).append("; ");
         }
         ApiError apiError = new ApiError("400", sb.toString());
+        log.error("an error occured",ex);
         return apiError;
     }
 
@@ -38,6 +44,15 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleHttpMessageNotReadable(HttpMessageNotReadableException ex){
         ApiError apiError = new ApiError("400","Malformed request");
+        log.error("an error occured",ex);
+        return apiError;
+    }
+
+    @ExceptionHandler(DateTimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleDateTime(DateTimeException ex){
+        ApiError apiError = new ApiError("400","Malformed Date");
+        log.error("an error occured",ex);
         return apiError;
     }
 
@@ -45,6 +60,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleException(Exception ex){
         ApiError apiError = new ApiError("500", ex.getMessage());
+        log.error("an error occured",ex);
         return apiError;
     }
 }
